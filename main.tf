3651a648540de041da75f0279522bbba35ba300e
bebyx <bebyx@protonmail.com>
2020-11-06 16:45:20 +0200

    Split oms in two files

---
+++ main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+
+provider "aws" {
+  region  = var.region
+}
+
+resource "aws_key_pair" "oms-ssh" {
+  key_name = "terraform"
+  public_key = file("~/.ssh/terraform.pub")
+}
+
+
+data "aws_ami" "redhat-linux-8" {
+  most_recent = true
+  owners      = ["309956199498"]
+  filter {
+    name   = "name"
+    values = ["RHEL-8.*"]
+  }
+
+  filter {
+   name   = "virtualization-type"
+   values = ["hvm"]
+  }
+
+  filter {
+   name   = "architecture"
+   values = ["x86_64"]
+ }
+}
+
+
+output "public_ip" {
+  value = aws_eip.ip-oms.public_ip
+}
+
+output "private_ip" {
+  value = aws_instance.oms.private_ip
+}
--- oms.tf
+++ oms.tf
@@ -1,48 +1,10 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 2.70"
-    }
-  }
-}
-
-provider "aws" {
-  region  = var.region
-}
-
-resource "aws_key_pair" "oms-ssh" {
-  key_name = "terraform"
-  public_key = file("~/.ssh/terraform.pub")
-}
-
-
-data "aws_ami" "redhat-linux-8" {
-  most_recent = true
-  owners      = ["309956199498"]
-  filter {
-    name   = "name"
-    values = ["RHEL-8.*"]
-  }
-
-  filter {
-   name   = "virtualization-type"
-   values = ["hvm"]
-  }
-
-  filter {
-   name   = "architecture"
-   values = ["x86_64"]
- }
-}
-
 resource "aws_instance" "oms" {
   ami           = data.aws_ami.redhat-linux-8.id
   instance_type = "t2.micro"
 
   key_name = aws_key_pair.oms-ssh.key_name
 
-  security_groups = [ aws_security_group.ingress-all-oms.id ]
+  security_groups = [ aws_security_group.oms-sg.id ]
   subnet_id = aws_subnet.oms-subnet.id
 
   tags = {
@@ -51,10 +13,20 @@ resource "aws_instance" "oms" {
 
 }
 
-output "public_ip" {
-  value = aws_eip.ip-oms.public_ip
-}
+/*
+resource "null_resource" "provision" {
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    private_key = file("~/.ssh/terraform")
+    host = aws_eip.ip-oms.public_ip
+  }
 
-output "private_ip" {
-  value = aws_instance.oms.private_ip
+  provisioner "remote-exec" {
+    inline = [
+      "sudo dnf install vim -y"
+    ]
+//    script = file()
+  }
 }
+*/
