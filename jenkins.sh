e320f37f407379d9e6c3581fa450dab0bf4308a1
bebyx <bebyx@protonmail.com>
2020-11-17 15:16:25 +0200

    Fix Jenkins plugin name

--- jenkins.sh
+++ jenkins.sh
@@ -7,11 +7,12 @@ curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | 
 sudo dnf install jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
-sleep 120
+sleep 240
 
 sudo wget http://localhost:8080/jnlpJars/jenkins-cli.jar
 echo 'jenkins.model.Jenkins.instance.securityRealm.createAccount("admin", "admin")' | sudo java -jar jenkins-cli.jar -s "http://localhost:8080" -auth admin:`sudo cat /var/lib/jenkins/secrets/initialAdminPassword` -noKeyAuth groovy =
-sudo java -jar jenkins-cli.jar -s "http://localhost:8080" -auth admin:admin install-plugin maven-plugin trilead-api jdk-tool workflow-support script-security command-launcher workflow-cps bouncycastle-api handlebars  locale javadoc momentjs structs workflow-step-api scm-api workflow-api junit apache-httpcomponents-client-4-api pipeline-input-step display-url-api mailer credentials ssh-credentials jsch maven-plugin git-server token-macro pipeline-stage-step run-condition matrix-project conditional-buildstep parameterized-trigger git git-client workflow-scm-step cloudbees-folder timestamper pipeline-milestone-step workflow-job jquery-detached jackson2-api branch-api ace-editor pipeline-graph-analysis pipeline-rest-api pipeline-stage-view pipeline-build-step plain-credentials credentials-binding pipeline-model-api pipeline-model-extensions workflow-cps-global-lib workflow-multibranch authentication-tokens docker-commons durable-task workflow-durable-task-step workflow-basic-steps docker-workflow pipeline-stage-tags-metadata pipeline-model-declarative-agent pipeline-model-definition workflow-aggregator lockable-resources github -deploy
+sleep 20
+sudo java -jar jenkins-cli.jar -s "http://localhost:8080" -auth admin:admin install-plugin trilead-api jdk-tool workflow-support script-security command-launcher workflow-cps bouncycastle-api handlebars locale javadoc momentjs structs workflow-step-api scm-api workflow-api junit apache-httpcomponents-client-4-api pipeline-input-step display-url-api mailer credentials ssh-slaves ssh-credentials jsch maven-plugin git-server token-macro pipeline-stage-step run-condition matrix-project conditional-buildstep parameterized-trigger git git-client workflow-scm-step cloudbees-folder timestamper pipeline-milestone-step workflow-job jquery-detached jackson2-api branch-api ace-editor pipeline-graph-analysis pipeline-rest-api pipeline-stage-view pipeline-build-step plain-credentials credentials-binding pipeline-model-api pipeline-model-extensions workflow-cps-global-lib workflow-multibranch authentication-tokens docker-commons durable-task workflow-durable-task-step workflow-basic-steps docker-workflow pipeline-stage-tags-metadata pipeline-model-declarative-agent pipeline-model-definition workflow-aggregator lockable-resources github -deploy
 
 sudo bash -c "cat <<-EOF > /var/lib/jenkins/hudson.tasks.Maven.xml
 <?xml version='1.1' encoding='UTF-8'?>
